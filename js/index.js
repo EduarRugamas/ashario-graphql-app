715948f7c5d0e7a2f581dbedaa439b6763224a7a
EduarRugamas <juaneduardo021299@hotmail.com>
2022-08-04 10:20:21 -0600

    intento de fetch a api de graphql ---> 1

--- js/index.js
+++ js/index.js
@@ -1,2 +1,41 @@
-//import { public_key, secret_key } from '../config/config.js';
+import {public_key, secret_key, url_base} from '../config/config.js';
 
+GetAllRetailerIds();
+
+
+
+const GetAllRetailerIds = () => {
+
+    const query_retailer = `
+    query GetRetailers {
+        retailers {
+           name,
+           id,
+           menuTypes,
+           address 
+        }
+    }
+    `
+
+
+    fetch(`${url_base}`, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": "Bearer6 " + public_key,
+        },
+        body: JSON.stringify({
+            query: query_retailer
+        })
+    })
+        .then( response => {
+            return response.json();
+        })
+        .then( data => {
+            console.log(data);
+        })
+        .catch( error => {
+            console.log(error.message);
+        })
+}
