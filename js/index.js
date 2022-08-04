2c8edbb29553d14f3ad42de6705e463b66e4dd6d
EduarRugamas <juaneduardo021299@hotmail.com>
2022-08-04 11:01:58 -0600

    intento de fetch a api de graphql ---> 5

--- js/index.js
+++ js/index.js
@@ -1,39 +1,5 @@
 import {public_key, secret_key, url_base} from '../config/config.js';
+import {GetAllRetailerIds} from '../utils/fetch_querys.js';
 
-const GetAllRetailerIds = () => {
-
-    const query_retailer = `
-    query GetRetailers {
-        retailers {
-           name,
-           id,
-           menuTypes,
-           address 
-        }
-    }
-    `
-
-
-    fetch(`${url_base}`, {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-            "Authorization": "Bearer " + public_key,
-        },
-        body: JSON.stringify({
-            query: query_retailer
-        })
-    })
-        .then( response => {
-            return response.json();
-        })
-        .then( data => {
-            console.log(data.retailers[0].name);
-        })
-        .catch( error => {
-            console.log(error.message);
-        })
-};
 
 GetAllRetailerIds();
---
+++ utils/fetch_querys.js
@@ -0,0 +1,39 @@
+import { secret_key, public_key, url_base } from '../config/config.js';
+
+const GetAllRetailerIds = () => {
+
+    const query_retailer = `
+    query GetRetailers {
+        retailers {
+           name,
+           id,
+           menuTypes,
+           address 
+        }
+    }
+    `
+    fetch(`${url_base}`, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": "Bearer " + public_key,
+        },
+        body: JSON.stringify({
+            query: query_retailer
+        })
+    })
+        .then( response => {
+            return response.json();
+        })
+        .then( data => {
+            console.log(data);
+        })
+        .catch( error => {
+            console.log(error.message);
+        })
+};
+
+export {
+    GetAllRetailerIds
+}
