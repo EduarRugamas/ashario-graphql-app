e6c5cb3f461b71bba232995792d408fd4489cafb
EduarRugamas <juaneduardo021299@hotmail.com>
2022-08-04 12:02:00 -0600

    intento de fetch a api de graphql ---> 7

--- utils/fetch_querys.js
+++ utils/fetch_querys.js
@@ -1,4 +1,6 @@
 import { secret_key, public_key, url_base } from '../config/config.js';
+const local_storage = window.localStorage;
+let stores = {};
 
 const GetAllRetailerIds = () => {
 
@@ -28,7 +30,55 @@ const GetAllRetailerIds = () => {
         })
         .then( data => {
             console.log(data);
-            console.log(data.data.retailers[0].name)
+            console.log(data.data.retailers[1].name);
+
+            if (local_storage.getItem('Ashario_stores')){
+                stores = JSON.parse(local_storage.getItem('Ashario_stores'));
+            }
+
+            let store_Centrepoint_Mall = {
+                "name": data.data.retailers[1].name,
+                "id": data.data.retailers[1].id,
+                "address": data.data.retailers[1].address,
+            }
+            let store_North_York = {
+                "name": data.data.retailers[2].name,
+                "id": data.data.retailers[2].id,
+                "address": data.data.retailers[2].address,
+            }
+
+            let store_Aurora = {
+                "name": data.data.retailers[3].name,
+                "id": data.data.retailers[3].id,
+                "address": data.data.retailers[3].address,
+            }
+
+            if (store_Centrepoint_Mall in stores) {
+                console.log('Tienda centre point mall');
+            }else {
+                stores[store_Centrepoint_Mall] = store_Centrepoint_Mall;
+                local_storage.setItem('Ashario_stores', JSON.stringify(stores));
+            }
+
+            if (store_North_York in stores) {
+                console.log('Tienda North York');
+            }else {
+                stores[store_North_York] = store_North_York;
+                local_storage.setItem('Ashario_stores', JSON.stringify(stores));
+            }
+
+            if (store_Aurora in stores) {
+                console.log('Tienda Aurora');
+            }else {
+                stores[store_Aurora] = store_Aurora;
+                local_storage.setItem('Ashario_stores', JSON.stringify(stores));
+            }
+
+            console.log(stores);
+
+
+        //    #1 Ashario_Centrepoint_Mall, #2 Ashario_North_York, #3 Ashario_Aurora
+
         })
         .catch( error => {
             console.log(error.message);
