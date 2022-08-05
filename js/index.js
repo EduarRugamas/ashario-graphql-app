dd0aae2a224f689e5eff385456e51b13106ce451
EduarRugamas <juaneduardo021299@hotmail.com>
2022-08-04 18:01:56 -0600

    intento de fetch a api de graphql ---> 15

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 import {public_key, secret_key, url_base} from '../config/config.js';
 import {GetAllProducts, GetAllRetailerIds} from '../utils/fetch_querys.js';
 const storage_local = window.localStorage;
-const retailerID_Ashario_Centrepoint_Mall = storage_local.getItem('Ashario_Centrepoint_Mall');
+const retailerID_Ashario_Centrepoint_Mall = JSON.parse(storage_local.getItem('Ashario_Centrepoint_Mall'));
 
-console.log(retailerID_Ashario_Centrepoint_Mall);
+console.log(retailerID_Ashario_Centrepoint_Mall.id);
 
 GetAllRetailerIds();
 
