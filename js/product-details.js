dad504c7e9453c7d41320c7f513356ba03a06343
EduarRugamas <juaneduardo021299@hotmail.com>
2022-08-05 10:14:54 -0600

    mostranndo card en vista index.html, informacion de api  graphql ---> 13

--- js/product-details.js
+++ js/product-details.js
@@ -3,3 +3,7 @@ const urlParams = new URLSearchParams(window.location.search);
 const storage_local = window.localStorage;
 const id_store_centre_point_mall = JSON.parse(storage_local.getItem('Ashario_Centrepoint_Mall'));
 const id_product = urlParams.get('id');
+
+
+
+GetProduct(id_store_centre_point_mall, id_product);
