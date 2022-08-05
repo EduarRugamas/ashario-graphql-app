500709213a13f34fbcfe211da9de375ed9323d85
EduarRugamas <juaneduardo021299@hotmail.com>
2022-08-04 19:39:02 -0600

    mostranndo card en vista index.html, informacion de api  graphql ---> 4

--- utils/fetch_querys.js
+++ utils/fetch_querys.js
@@ -131,41 +131,9 @@ const GetAllProducts = (retailerID) => {
         .then(data => {
             console.log(data);
 
-            data.data.menu.products.map(item => {
-                container_products.innerHTML = `
-            
-             <div class="col">
-            <div class="card rounded-0 product-card">
-                    <div class="card-body">
-                        <div class="product-info">
-                            <a href="product-details.html?objectID=">
-                                <p class="product-catergory font-13 mb-1 itembrand"></p>
-                                <p class="product-catergory font-13 mb-1 itemsubtype" id="itemsubtype"></p>
-                            </a>
-                            <a href="product-details.html?objectID=">
-                                <h6 class="product-name mb-2 itemname">${item.name}</h6>
-                            </a>
-                            <div class="d-flex align-items-center">
-                                <div class="mb-1 product-price itemprice jcitemprice">
-                                    <span class="fs-5 currencyformat jcpriceformat">CAD </span>
-                                    <span class="fs-5 jcpricingnw"></span>
-                                    <span class="er-each jceachformat" style="align-items: flex-end;"></span>
-                                </div>
-                            </div>
-                            <div class="product-action mt-2" id="content">
-                               <div class="d-grid gap-2">
-                                    <a class="btn btn-dark btn-ecomm" id="add_to_cart_btn"><i class="bx bxs-cart-add"></i>add to cart</a>
-                                    <a href="/views/product-details.html?objectID=" class="btn btn-light btn-ecomm">Product Details</a>
-                               </div> 
-                            </div> 
-                        </div>
-                    </div>
-                </div>
-        </div>
-            
-            
-            `
-            }).join('');
+            let result = data.data.menu.products;
+            console.log(result);
+
 
 
         }).catch(error => console.log(error.message));
@@ -176,3 +144,35 @@ export {
     GetAllRetailerIds,
     GetAllProducts
 }
+
+
+// <div className="col">
+//     <div className="card rounded-0 product-card">
+//         <div className="card-body">
+//             <div className="product-info">
+//                 <a href="product-details.html?objectID=">
+//                     <p className="product-catergory font-13 mb-1 itembrand"></p>
+//                     <p className="product-catergory font-13 mb-1 itemsubtype" id="itemsubtype"></p>
+//                 </a>
+//                 <a href="product-details.html?objectID=">
+//                     <h6 className="product-name mb-2 itemname">${item.name}</h6>
+//                 </a>
+//                 <div className="d-flex align-items-center">
+//                     <div className="mb-1 product-price itemprice jcitemprice">
+//                         <span className="fs-5 currencyformat jcpriceformat">CAD </span>
+//                         <span className="fs-5 jcpricingnw"></span>
+//                         <span className="er-each jceachformat" style="align-items: flex-end;"></span>
+//                     </div>
+//                 </div>
+//                 <div className="product-action mt-2" id="content">
+//                     <div className="d-grid gap-2">
+//                         <a className="btn btn-dark btn-ecomm" id="add_to_cart_btn"><i className="bx bxs-cart-add"></i>add
+//                             to cart</a>
+//                         <a href="/views/product-details.html?objectID=" className="btn btn-light btn-ecomm">Product
+//                             Details</a>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     </div>
+// </div>
