dcb9b15db2bf6eb9f6205b32efedf290967b591c
EduarRugamas <juaneduardo021299@hotmail.com>
2022-08-11 21:59:25 -0600

    solucionando bugs --> 4

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ window.addEventListener('DOMContentLoaded', () => {
         const Ashario_Centre_point_Mall = JSON.parse(storage_local.getItem('Ashario_Centrepoint_Mall'));
 
         const data = getAllProducts(Ashario_Centre_point_Mall.id);
-        console.log(data.data.menu);
+        console.log(data.menu);
 
     //
     //     GetAllProducts(Ashario_Centre_point_Mall.id).then( (items) => {
