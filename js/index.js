99ed21daf3be7bcadad568f583a4da3bbe6867a9
EduarRugamas <juaneduardo021299@hotmail.com>
2022-08-19 11:38:57 -0600

    funcion pruebas de filtros --> 3

--- js/index.js
+++ js/index.js
@@ -157,12 +157,12 @@ window.addEventListener('DOMContentLoaded', async () => {
             
              <div class="col">
                 <div class="card rounded-0 product-card">
-                        <a href="/views/product-details.html?objectID=${product.id}" id="container_carrousel_imgs">
+                        <a href="/views/product-details.html?id=${product.id}" id="container_carrousel_imgs">
                             <img src="${product.image !== null ? product.image : '../assets/images/errors-images/image-not-found.jpeg'}" class="card-img-top" alt="${product.name}" id="imagen-product">
                         </a>
                     <div class="card-body">
                         <div class="product-info">
-                            <a href="product-details.html?objectID=${product.id}">
+                            <a href="product-details.html?id=${product.id}">
                                 <p class="product-catergory font-13 mb-1 itembrand">${product.brand}</p>
                                 <p class="product-catergory font-13 mb-1 itemsubtype" id="itemsubtype">null</p>
                             </a>
@@ -179,7 +179,7 @@ window.addEventListener('DOMContentLoaded', async () => {
                             <div class="product-action mt-2" id="content">
                                <div class="d-grid gap-2">
                                     <a class="btn btn-dark btn-ecomm" id="add_to_cart_btn" ><i class="bx bxs-cart-add"></i>add to cart</a>
-                                    <a href="/views/product-details.html?objectID=${product.id}" class="btn btn-light btn-ecomm">Product Details</a>
+                                    <a href="/views/product-details.html?id=${product.id}" class="btn btn-light btn-ecomm">Product Details</a>
                                </div> 
                             </div> 
                         </div>
