5ed2b34b869ba7794fc3842130324031aa5d178d
EduarRugamas <juaneduardo021299@hotmail.com>
2022-08-20 08:48:03 -0600

    funcion pruebas de filtros --> 29

--- js/index.js
+++ js/index.js
@@ -1,9 +1,7 @@
 import {filter_strain_type_lineage, getAllProducts, GetAllRetailerIds} from '../utils/querys.js';
 import { createElementHtml, appendElementHtml } from '../utils/elements_html.js';
 const container_products = document.querySelector('#container-products');
-let groupRadio = document.getElementsByName('filter_lineage');
-
-
+const groupRadio = document.getElementsByName('filter_lineage');
 
 const storage_local = window.localStorage;
 
@@ -12,54 +10,46 @@ window.addEventListener('DOMContentLoaded', async () => {
         await GetAllRetailerIds();
 
         setTimeout(async () => {
+            let position;
 
             let radio_all = document.querySelector('#filter_all_lineage');
             const store_centre_point_mall = JSON.parse(storage_local.getItem('Ashario_Centrepoint_Mall'));
 
             let data = await getAllProducts(store_centre_point_mall.id);
-
             if (radio_all.checked && radio_all.value === 'all'){
                 renderProductAll(container_products, data.products);
             }
 
 
-            groupRadio.forEach(  (radio) => {
-                radio.addEventListener('change', () => {
-
-                    if (radio.checked  && radio.value === 'all') {
-                        const store_centre_point_mall = JSON.parse(storage_local.getItem('Ashario_Centrepoint_Mall'));
-                        let data = getAllProducts(store_centre_point_mall.id);
-                        renderProductAll(container_products, data.products);
-                    }else if (radio.value === 'indica' && radio.checked) {
-                        const store_centre_point_mall = JSON.parse(storage_local.getItem('Ashario_Centrepoint_Mall'));
-                        let filter_indica = filter_strain_type_lineage(store_centre_point_mall.id, 'indica');
-                        createProductFilter(container_products, filter_indica.products);
-                    }else if (radio.value === 'sativa' && radio.checked){
-                        const store_centre_point_mall = JSON.parse(storage_local.getItem('Ashario_Centrepoint_Mall'));
-                        let filter_sativa = filter_strain_type_lineage(store_centre_point_mall.id, 'sativa');
-                        createProductFilter(container_products, filter_sativa.products);
-                    }else if (radio.value === 'hybrid' && radio.checked) {
-                        const store_centre_point_mall = JSON.parse(storage_local.getItem('Ashario_Centrepoint_Mall'));
-                        let filter_hybrid =  filter_strain_type_lineage(store_centre_point_mall.id, 'hybrid');
-                        createProductFilter(container_products, filter_hybrid.products);
-                    }else if (radio.value === 'high_cbd' && radio.checked) {
-                        console.log('entro a high_cbd');
-                        const store_centre_point_mall = JSON.parse(storage_local.getItem('Ashario_Centrepoint_Mall'));
-                        let filter_high_cbd = filter_strain_type_lineage(store_centre_point_mall.id, 'high_cbd');
-                        createProductFilter(container_products, filter_high_cbd.products);
-                    }else if (radio.value === 'not_applicable' && radio.checked) {
-                        console.log('entro a not applicable');
-                        const store_centre_point_mall = JSON.parse(storage_local.getItem('Ashario_Centrepoint_Mall'));
-                        let filter_not_applicable = filter_strain_type_lineage(store_centre_point_mall.id, 'not_applicable');
-                        createProductFilter(container_products, filter_not_applicable.products);
-                    }
-                })
-            });
-
         }, 100);
 
+    let filter_indica = await filter_strain_type_lineage(store_centre_point_mall.id, 'indica');
+    let filter_sativa = await filter_strain_type_lineage(store_centre_point_mall.id, 'sativa');
+    let filter_hybrid = await filter_strain_type_lineage(store_centre_point_mall.id, 'hybrid');
+    let filter_high_cbd = await filter_strain_type_lineage(store_centre_point_mall.id, 'high_cbd');
+    let filter_not_applicable = await filter_strain_type_lineage(store_centre_point_mall.id, 'not_applicable');
 
 
+    groupRadio.forEach( (radio) => {
+        radio.addEventListener('change', () => {
+
+            if (radio.checked  && radio.value === 'all') {
+                renderProductAll(container_products, data.products);
+            }
+            if (radio.value === 'indica' && radio.checked) {
+                createProductFilter(container_products, filter_indica.products);
+            }else if (radio.value === 'sativa' && radio.checked){
+                createProductFilter(container_products, filter_sativa.products);
+            }else if (radio.value === 'hybrid' && radio.checked) {
+                createProductFilter(container_products, filter_hybrid.products);
+            }else if (radio.value === 'high_cbd' && radio.checked) {
+                createProductFilter(container_products, filter_high_cbd.products);
+            }else if (radio.value === 'not_applicable' && radio.checked) {
+                console.log('entro a not applicable');
+                createProductFilter(container_products, filter_not_applicable.products);
+            }
+        })
+    });
 
 
 
