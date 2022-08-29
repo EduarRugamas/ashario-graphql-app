2b483b5848d931221a06a0a97d0511a21e0eadce
EduarRugamas <juaneduardo021299@hotmail.com>
2022-08-28 19:53:50 -0600

    agregando view de quantity y weighth en grid principal 5

--- js/index.js
+++ js/index.js
@@ -576,7 +576,7 @@ window.addEventListener('DOMContentLoaded', async () => {
                                 </div>
                             </div>
                             
-                            <div class="product-action mt-2" id="content">
+                            <div class="product-action" id="content">
                                <div class="d-grid gap-2">
                                     <a class="btn btn-dark btn-ecomm" id="add_to_cart_btn" ><i class="bx bxs-cart-add"></i>add to cart</a>
                                     <a href="/views/product-details.html?id=${product.id}" class="btn btn-light btn-ecomm">Product Details</a>
