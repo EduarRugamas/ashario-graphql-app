bf952fa3f3cb44808c501fc7baca5206d82e50c2
EduarRugamas <juaneduardo021299@hotmail.com>
2022-08-29 16:51:08 -0600

    agregando view de quantity y weighth en grid principal 54

--- js/index.js
+++ js/index.js
@@ -697,7 +697,7 @@ window.addEventListener('DOMContentLoaded', async () => {
         });
         container_select_weights.forEach(items => {
             const get_weights = items.getAttribute('product_variants');
-            console.table(get_weights);
+            console.log(JSON.parse(get_weights));
         });
 
     }
