479f189c4a1d8c12673ea4760165d037b94f484c
EduarRugamas <juaneduardo021299@hotmail.com>
2022-08-31 22:47:12 -0600

    solucionando bugs de grid principal 48

--- js/index.js
+++ js/index.js
@@ -805,7 +805,8 @@ window.addEventListener('DOMContentLoaded', async () => {
             console.log(get_value_weights);
 
             if (get_value_weights === '3.5g') {
-                let weight_product_variant_35 = array_products.products.variants.find(item => item.option === get_value_weights);
+                const variantes = array_products.products.variants;
+                let weight_product_variant_35 = variantes.find(item => item.option === get_value_weights);
                 console.log('encontre variante 3.5g', weight_product_variant_35);
             }
 
