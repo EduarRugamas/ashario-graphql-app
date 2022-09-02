96721eb22006b897540a1f6023e1e4abc338ba95
EduarRugamas <juaneduardo021299@hotmail.com>
2022-09-01 18:20:57 -0600

    agregando busqueda de searchh cuando se escribe 19

--- js/index.js
+++ js/index.js
@@ -761,12 +761,11 @@ window.addEventListener('DOMContentLoaded', async () => {
             result.then( response => {
                 if (response.products.length === 0 ){
                     console.log('sin resultados');
-                    FadeIn(div_loader);
-
+                    ViewEmpty(container_products);
                 }
                 console.log(response.products);
                 cartProduct(container_products, response.products);
-                FadeOut(div_loader);
+
 
 
             }).catch(error => {
@@ -789,13 +788,11 @@ window.addEventListener('DOMContentLoaded', async () => {
             result.then( response => {
                 if (response.products.length === 0 ){
                     console.log('sin resultados');
-                    FadeIn(div_loader);
                     ViewEmpty(container_products);
                 }
-                    console.log(response);
-                    FadeIn(div_loader);
-                    cartProduct(container_products, response.products);
-                    FadeOut(div_loader);
+                console.log(response);
+                cartProduct(container_products, response.products);
+
 
 
             }).catch(error => {
