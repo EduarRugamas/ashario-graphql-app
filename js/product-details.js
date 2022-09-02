a12dec54ffbcad0787dc10a6763f1f270d9606b0
EduarRugamas <juaneduardo021299@hotmail.com>
2022-09-01 20:10:08 -0600

    product details mejoraando performans 6

--- js/product-details.js
+++ js/product-details.js
@@ -488,7 +488,7 @@ const renderProduct = (container, informatio_product) => {
                                                 <dt class="col-sm-3">Product id</dt>
                                                 <dd class="col-sm-9"># ${informatio_product.id}</dd>
                                                 <dt class="col-sm-3">Stant</dt>
-                                                <dd class="col-sm-9 bg-badge-strant">${informatio_product.strainType}</dd>
+                                                <dd class="col-sm-9 badge bg-badge-strant">${informatio_product.strainType}</dd>
                                             </dl>
                                             <div class="d-flex" id="content_effects" style="margin-top: -10px; margin-bottom: 20px;"></div>
                                             <h6>Description:</h6>
