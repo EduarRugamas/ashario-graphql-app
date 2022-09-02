5477c76dc3b85e4eb12c8282dc772f4d0c154a47
EduarRugamas <juaneduardo021299@hotmail.com>
2022-09-02 14:09:48 -0600

    custom alert 3

--- js/product-details.js
+++ js/product-details.js
@@ -880,7 +880,6 @@ const render_add_item_cart = (store_id, cart_id, product_id, id_select_quantity,
                         icon: 'error',
                         text: `Sorry! You've reached the 30g purchase limit for cannabis due to provincial regulations.`,
                         confirmButtonColor: '#3e3e3e',
-                        customClass: 'sweetAlertbtn'
                     });
                 }
 
