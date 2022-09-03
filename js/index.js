33b680907d60c0e47e69e03f111aeda8a31447e9
EduarRugamas <juaneduardo021299@hotmail.com>
2022-09-03 14:02:06 -0600

    solucionando bugs de grid 15

--- js/index.js
+++ js/index.js
@@ -513,7 +513,7 @@ window.addEventListener('DOMContentLoaded', async () => {
             const get_element_strain = document.getElementById('badge-straint-' + id_product);
             console.log('elements',get_element_strain);
 
-            const get_information_strainType = array_products.find(item => item.productId === id_product);
+            const get_information_strainType = array_products.find(item => item.id === id_product);
             console.log(get_information_strainType);
         });
     }
