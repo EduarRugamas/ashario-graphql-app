f477a02ee09565a9314e1e41a5f803e07f1fd2e2
EduarRugamas <juaneduardo021299@hotmail.com>
2022-09-03 14:51:31 -0600

    solucionando bugs de grid 27

--- js/index.js
+++ js/index.js
@@ -304,7 +304,7 @@ window.addEventListener('DOMContentLoaded', async () => {
                     <div class="card-body">
                         <div class="product-info">
                             <a href="/views/product-details.html?id=${product.id}" id="content_text_brand">
-                                <p class="product-catergory font-13 mb-1 content-badge-strain justify-content-center align-content-center align-items-center text-center" badge_id="${product.id}" id="badge-straint-${product.id}"></p>
+                                <p class="product-catergory font-13 mb-1 content-badge-strain" badge_id="${product.id}" id="badge-straint-${product.id}"></p>
                                 <p class="product-catergory font-13 mb-1 itembrand text-center">${product.brand.name}</p>
                                 <p class="product-catergory font-13 mb-1 itemsubtype" id="itemsubtype"></p>
                             </a>
