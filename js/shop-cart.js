17702ff5e8f201e27d35b1506938b942c35568ee
EduarRugamas <juaneduardo021299@hotmail.com>
2022-09-08 18:52:18 -0600

    page checkout shop cart 11

--- js/shop-cart.js
+++ js/shop-cart.js
@@ -54,7 +54,7 @@ window.addEventListener('DOMContentLoaded', async () => {
                         </div>
                     </div>`;
             contenedor_products.innerHTML = template_empty_mini_cart;
-            btn_checkout_cart.className = 'btn btn-dark btn-ecomm disabled';
+            btn_checkout_cart.classList.add('disabled');
         } else {
             render_products_cart(contenedor_products, data.products);
             update_icon_cart();
