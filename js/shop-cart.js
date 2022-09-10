ce8ede0172a197400de997565c2f5a48cc5dcdb6
EduarRugamas <juaneduardo021299@hotmail.com>
2022-09-10 12:12:23 -0600

    page checkout shop cart 16

--- js/shop-cart.js
+++ js/shop-cart.js
@@ -84,19 +84,20 @@ window.addEventListener('DOMContentLoaded', async () => {
             btn_checkout_cart.addEventListener('click', async () => {
 
                 console.log(cart);
-                console.log(checkoutId);
 
-                // for (let item in cart) {
-                //
-                //     console.log(item);
-                //
-                //     console.log(cart[item].value_quantity);
-                //     console.log(cart[item].value_weight);
-                //
-                //     await addItemCart(id_store_centre_point_mall.id, checkoutId.id, cart[item].product_id, cart[item].value_quantity, cart[item].value_weight).then(result => {
-                //         window.location.href = `${checkoutId.redirectUrl}`;
-                //     });
-                // }
+
+                for (let item in cart) {
+
+                    console.log(item);
+
+                    console.log(cart[item].value_quantity);
+                    console.log(cart[item].value_weight);
+
+                    await addItemCart(id_store_centre_point_mall.id, checkoutId.id, cart[item].product_id, cart[item].value_quantity, cart[item].value_weight).then(result => {
+                        window.location.href = `${checkoutId.redirectUrl}`;
+                        console.log(checkoutId);
+                    });
+                }
 
                 // for (let item in cart) {
                 //     console.log(item);
